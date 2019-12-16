163314854e5b4959a24bd0c3c29b30c3e54de836
Brian Landvogt <brianlandvogt@Brians-MacBook-Pro.local>
2019-12-16 01:46:47 -0500

    Changed icon image in config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/images/meSuit.png`, // This path is relative to the root of the site.
+        icon: `src/images/meSuit2.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
